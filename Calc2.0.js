440174148d384368220d3a00a8cbc724c7d06f86
433doro <doroktan@gmail.com>
2018-02-17 22:39:39 +0200

    Calculator works!

--- Calc2.0.js
+++ Calc2.0.js
@@ -1,9 +1,32 @@
+/** 
+*Collects the value of an DOM object which triggered a 'click'
+* @param {event} event the te click event 
+*calls the CheckUserInput and supplaies the value of the DOM Object as a param
+ */
 function collectDataFromDOM(event) {
   var clickInput = event.target.value;
   checkUserInput(clickInput);
 }
 
+
+function dislplayCheck(DOMInputLength,Input)
+ {
+  var DOMprePresent = [12]
+   if (DOMInputSize>12)
+   { 
+     for (let i = DOMInputSize [Input.length-12]; i < DOMInputSize.length; i++)
+     {
+       
+
+
+
+     }
+
+   }
+
+
 function checkUserInput(clickInput) {
+  var DOMInputSize = DOMInput.length
   var DOMPresentation;
   if (clickInput >= 0 && clickInput <= 9) {
     checkOnlyTwoOpersINaROW = 0;
@@ -13,7 +36,7 @@ function checkUserInput(clickInput) {
   } else if (clickInput == "delete") {
     DOMInput = [];
     document.querySelector("#result").innerHTML = "0";
-  } else if (clickInput !== "=" && checkOnlyTwoOpersINaROW <= 2) {
+  } else if (clickInput !== "=" && checkOnlyTwoOpersINaROW < 2) {
     DOMInput.push(clickInput);
     DOMPresentation = DOMInput.join("");
     document.querySelector("#result").innerHTML = DOMPresentation;
@@ -21,12 +44,12 @@ function checkUserInput(clickInput) {
   } else if (clickInput === "=") {
     DOMPresentation = DOMInput.join("");
     expression = DOMPresentation.split(/([\/\*\+\-])/);
-    result = Calculate(checkNegatives(removeWhiteSpaces(expression)));
-    if(isNaN(result)){
-    document.querySelector("#result").innerHTML = "Invalid Input";
-    }
-    else{
-        document.querySelector("#result").innerHTML = result;
+    var x = removeWhiteSpaces(expression);
+    result = Calculate(checkNegatives(x));
+    if (isNaN(result)) {
+      document.querySelector("#result").innerHTML = "Invalid Input";
+    } else {
+      document.querySelector("#result").innerHTML = result;
     }
   }
 }
@@ -42,7 +65,15 @@ function removeWhiteSpaces(splittedUserInput) {
 
 function checkNegatives(noWhiteSpaces) {
   for (i = 0; i < noWhiteSpaces.length; i++) {
-    if (i != 0 && noWhiteSpaces[i] !== "-") {
+    if (i === 0 && noWhiteSpaces[i] === "-") {
+      var negativeNumber;
+      var nextNumber = noWhiteSpaces[i + 1];
+      var minusOperator = noWhiteSpaces[i];
+
+      negativeNumber = parseFloat(minusOperator + nextNumber);
+      noWhiteSpaces[i + 1] = negativeNumber;
+      noWhiteSpaces[i] = 0;
+    } else {
       var firstBuffer = noWhiteSpaces[i];
       var secondBuffer = noWhiteSpaces[i + 1];
       var nextNumber = noWhiteSpaces[i + 2];
@@ -69,14 +100,6 @@ function checkNegatives(noWhiteSpaces) {
         operator = noWhiteSpaces[i];
         fixedMathExpression.push(operator);
       }
-    } else {
-      var negativeNumber;
-      var nextNumber = noWhiteSpaces[i + 1];
-      var minusOperator = noWhiteSpaces[i];
-
-      negativeNumber = parseFloat(minusOperator + nextNumber);
-      noWhiteSpaces[i + 1] = negativeNumber;
-      noWhiteSpaces[i] = 0;
     }
   }
   return fixedMathExpression;
@@ -84,9 +107,15 @@ function checkNegatives(noWhiteSpaces) {
 
 function Calculate(fixedExpression) {
   for (i = 0; i <= fixedExpression.length - 2; i++) {
-    switch (fixedExpression[i]) {
+    var elementOfFixedExp = fixedExpression[i];
+    switch (elementOfFixedExp) {
       case "+":
         result = fixedExpression[i - 1] + fixedExpression[i + 1];
+        fixedExpression[i + 1] = result;
+        break;
+
+      case "-":
+        result = fixedExpression[i - 1] - fixedExpression[i + 1];
         fixedExpression[i + 1] = result;
         break;
 
@@ -108,20 +137,25 @@ function Calculate(fixedExpression) {
 }
 
 var fixedMathExpression = [],
+  noWhiteSpaces = [],
+  splittedUserInput = [],
   splittedByOperatorsWithoutSpaces = [],
   splittedByOperators = [],
-  userInput = "";
-var fixeExpression = 0,
-  result = 0;
+  userInput = "",
+  DOMInput = [],
+  expression = [];
+
+var fixedExpression = 0,
+  result = 0,
+  checkOnlyTwoOpersINaROW = 0,
+  checkOnlyTwoOpersINaROW = 0;
+
 var firstRow = document.getElementById("first-rows");
-var DOMInput = [];
-var checkOnlyTwoOpersINaROW = 0;
-var expression = [];
+var numbers = document.getElementById("numbers");
 numbers.addEventListener("click", collectDataFromDOM);
 firstRow.addEventListener("click", collectDataFromDOM);
 
 splittedByOperators = userInput.split(/([\/\*\+\-])/);
-
 fixeExpression = checkNegatives(removeWhiteSpaces(splittedByOperators));
 result = Calculate(fixeExpression);
-alert(result);
+alert("Start caclulating");
