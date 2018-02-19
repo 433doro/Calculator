2064c38c3675c6b545374761e5b8499bb4c20ef6
433doro <doroktan@gmail.com>
2018-02-20 00:56:01 +0200

    minor change at the test file

--- Autotest.py
+++ Autotest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from time import sleep
 
 class SimpleCalcNoNegatives(unittest.TestCase):
 
@@ -13,13 +14,21 @@ class SimpleCalcNoNegatives(unittest.TestCase):
         driver = self.driver
         driver.get("C:/Users/Dell/Documents/Calculator/index.HTML")
         
-        WebDriverWait(driver,5).until((EC.presence_of_element_located((By.ID,"9")))).click()
-        WebDriverWait(driver,5).until((EC.presence_of_element_located((By.ID,"-")))).click()
+        WebDriverWait(driver,5).until((EC.presence_of_element_located((By.ID,"7")))).click()
+        
+        WebDriverWait(driver,5).until((EC.presence_of_element_located((By.XPATH,"//button[@value='0']")))).click()
+        
+        WebDriverWait(driver,5).until((EC.presence_of_element_located((By.XPATH,"//button[@value='+']")))).click()
+        
         WebDriverWait(driver,5).until((EC.presence_of_element_located((By.ID,"2")))).click()
+        
+        WebDriverWait(driver,5).until((EC.presence_of_element_located((By.ID,"6")))).click()
+        
         WebDriverWait(driver,5).until((EC.presence_of_element_located((By.ID,"eqn-bg")))).click()
+        
         element = driver.find_element_by_id("result")
         result = int(element.text)
-        assert  7 not in result
+        assert  70 + 26 == result, "Fuck yeah!"
 
 
     def tearDown(self):
